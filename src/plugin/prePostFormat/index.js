a3457521a696e7d1bc4358e2db9004a81b876743
Moshe Jonathan Gordon Radian <vehpus@gmail.com>
2020-11-11 02:15:12 +0200

    Added plugin template with some links for development resources (also in the Trello card)

---
+++ src/plugin/prePostFormat/index.js
@@ -0,0 +1,26 @@
+// Plugin template from https://day.js.org/docs/en/plugin/plugin
+
+// https://github.com/moment/moment/blob/develop/src/lib/moment/format.js
+// https://github.com/moment/moment/blob/e047716131e9f1650504a194b11b5405b098c603/src/test/moment/preparse_postformat.js
+// https://github.com/moment/moment/blob/b7ec8e2ec068e03de4f832f28362675bb9e02261/moment.js
+// https://github.com/moment/moment/blob/develop/src/locale/ar.js
+
+export default (option, dayjsClass, dayjsFactory) => {
+    // extend dayjs()
+    // e.g. add dayjs().isSameOrBefore()
+    dayjsClass.prototype.isSameOrBefore = function(arguments) {}
+  
+    // extend dayjs
+    // e.g. add dayjs.utc()
+    dayjsFactory.utc = arguments => {}
+  
+    // overriding existing API
+    // e.g. extend dayjs().format()
+    const oldFormat = dayjsClass.prototype.format
+    dayjsClass.prototype.format = function(arguments) {
+      // original format result
+      const result = oldFormat.bind(this)(arguments)
+      // return modified result
+    }
+  }
+  
